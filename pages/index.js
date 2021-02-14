f0a2640bcbd8d2e4ee2630f8bf15720b6ea1fbcf
Wahab Shaikh <wahab.shaikh@somaiya.edu>
2021-02-14 17:32:18 +0530

    🚀 Next.js & Tailwind CSS Setup

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Wahab Shaikh</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Wahab Shaikh</h1>
+    </>
+  );
+};
+
+export default Home;
